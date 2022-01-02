f81eba240d42223f982265997dc3b366aa8f3521
Ren Jiaqi <74390368+jren9982@users.noreply.github.com>
2022-01-02 13:05:55 +0800

    updated implementation of frontend

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,3 +1,8 @@
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyC9axacpLu2YnER2U1JsX0INoP8-CPtifQ",
     authDomain: "whatsapp-mern-fb917.firebaseapp.com",
@@ -7,3 +12,10 @@ const firebaseConfig = {
     appId: "1:508929995481:web:540280a5cde937490404c8"
   };
   
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
